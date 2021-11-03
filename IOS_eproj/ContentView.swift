c29b5b9355e131b42a9b593bea6ae9ccb533eb2d
frederiklc <fchri12@student.sdu.dk>
2021-11-03 08:15:02 +0100

    Initial Commit

---
+++ IOS_eproj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_eproj
+//
+//  Created by Frederik Lindvig Christensen on 03/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS_eproj/IOS_eprojApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_eprojApp.swift
+//  IOS_eproj
+//
+//  Created by Frederik Lindvig Christensen on 03/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct IOS_eprojApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
