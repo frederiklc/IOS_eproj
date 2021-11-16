4a2f0d95a263c55f66c8f5dcb72ac9dd625b59fc
Kristian <krand17@student.sdu.dk>
2021-11-16 12:07:25 +0100

    Added pages

---
+++ IOS_eproj/View/AddIngredient.swift
@@ -0,0 +1,20 @@
+//
+//  AddIngredient.swift
+//  IOS_eproj
+//
+//  Created by Kristian Andersen on 16/11/2021.
+//
+
+import SwiftUI
+
+struct AddIngredient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddIngredient_Previews: PreviewProvider {
+    static var previews: some View {
+        AddIngredient()
+    }
+}
---
+++ IOS_eproj/View/YourIngredients.swift
@@ -0,0 +1,20 @@
+//
+//  YourIngredients.swift
+//  IOS_eproj
+//
+//  Created by Kristian Andersen on 16/11/2021.
+//
+
+import SwiftUI
+
+struct YourIngredients: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct YourIngredients_Previews: PreviewProvider {
+    static var previews: some View {
+        YourIngredients()
+    }
+}
